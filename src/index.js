791d2fbb3f8ebe99411e2de0c998d085592034ac
Raushaniam <mullakhmetova18@gmail.com>
2022-08-08 13:26:52 +0400

    feat: implement task "Brackets"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const bracketsPair = {}
+
+  bracketsConfig.forEach((item) => {
+    openBrackets.push(item[0]);
+  })
+
+  bracketsConfig.forEach((item) => {
+    bracketsPair[item[1]] = item[0]
+  })
+
+  let stack = [];
+  let count = 0;
+  let count1 = 0;
+  for (let i = 0; i < str.length; i++) {
+    let symbol = str[i];
+
+    if (openBrackets.includes(symbol)) {
+      if (symbol === '|' || symbol === '7' || symbol === '8') {
+        count++;
+      }
+      if (symbol === '8') {
+        count1++;
+      }
+      if ((symbol === '|' || symbol === '7') && count % 2 === 0) {
+        stack.pop();
+      } else if(symbol === '8' && count1 % 2 === 0) {
+        stack.pop();
+      } else {
+        stack.push(symbol);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let topElem = stack[stack.length - 1];
+      if (bracketsPair[symbol] === topElem) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
